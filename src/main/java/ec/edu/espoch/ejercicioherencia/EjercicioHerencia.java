541fd1782ed90a69071a275245eeb672972aa00e
Zanyn-user <zanynuser@gmail.com>
2024-12-18 14:59:11 -0500

    Agregue contenido nuevo

--- src/main/java/ec/edu/espoch/ejercicioherencia/EjercicioHerencia.java
+++ src/main/java/ec/edu/espoch/ejercicioherencia/EjercicioHerencia.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package ec.edu.espoch.ejercicioherencia;
 
-/**
- *
- * @author SO-LAB1-PC22
- */
+import ec.edu.espoch.ejercicioherencia.clases.CuentaBancaria;
+
+
 public class EjercicioHerencia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("=== Prueba de Cuenta ===");
+        CuentaBancaria cuenta = new CuentaBancaria(50000, 12);
+        cuenta.consignar(20000);
+        cuenta.retirar(15000);
+        cuenta.calcularInteresMensual();
+        cuenta.extractoMensual();
+        cuenta.imprimir();
+
+        System.out.println("\n=== Prueba de CuentaAhorros ===");
+        CuentaAhorros cuentaAhorros = new CuentaAhorros(12000, 10);
+        cuentaAhorros.consignar(5000);
+        cuentaAhorros.retirar(3000);
+        cuentaAhorros.retirar(2000);
+        cuentaAhorros.retirar(1500);
+        cuentaAhorros.retirar(1000);
+        cuentaAhorros.retirar(500); // Genera comisión
+        cuentaAhorros.extractoMensual();
+        cuentaAhorros.imprimir();
     }
 }
+

30a12449abdf3e6a1ed34326539d04577154e7c1
Zanyn-user <zanynuser@gmail.com>
2024-12-18 13:39:30 -0500

    Primer comentario

---
+++ src/main/java/ec/edu/espoch/ejercicioherencia/EjercicioHerencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.ejercicioherencia;
+
+/**
+ *
+ * @author SO-LAB1-PC22
+ */
+public class EjercicioHerencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

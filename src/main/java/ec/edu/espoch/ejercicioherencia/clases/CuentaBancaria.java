82cadacc45b1c93122c3de253f0e6010ab809858
sadia-pao <aandiyumbo@gmail.com>
2024-12-18 14:29:02 -0500

    creacion de la clase bancaria

--- src/main/java/ec/edu/espoch/ejercicioherencia/clases/CuentaBancaria.java
+++ src/main/java/ec/edu/espoch/ejercicioherencia/clases/CuentaBancaria.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espoch.ejercicioherencia.clases;
 
-/**
- *
- * @author SO-LAB1-PC21
- */
 public class CuentaBancaria {
+    
+    protected float saldo;
+    protected int numConsignaciones = 0;
+    protected int numRetiros = 0;
+    protected float tasaAnual;
+    protected float comMensual = 0;
+
+    public CuentaBancaria(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumConsignaciones() {
+        return numConsignaciones;
+    }
+
+    public void setNumConsignaciones(int numConsignaciones) {
+        this.numConsignaciones = numConsignaciones;
+    }
+
+    public int getNumRetiros() {
+        return numRetiros;
+    }
+
+    public void setNumRetiros(int numRetiros) {
+        this.numRetiros = numRetiros;
+    }
+
+    public float getTasaAnual() {
+        return tasaAnual;
+    }
+
+    public void setTasaAnual(float tasaAnual) {
+        this.tasaAnual = tasaAnual;
+    }
+
+    public float getComMensual() {
+        return comMensual;
+    }
+
+    public void setComMensual(float comMensual) {
+        this.comMensual = comMensual;
+    }
+    
+    
     
 }
